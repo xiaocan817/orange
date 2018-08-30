a8772cc62df263a3fe1d6d72c4051ad862bfd1f5
xiaocan <xiaocan817@163.com>
2018-08-30 10:23:44 +0800

    test jenkins

--- src/main/java/com/atguigu/ci/entities/Orange.java
+++ src/main/java/com/atguigu/ci/entities/Orange.java
@@ -2,7 +2,7 @@ package com.atguigu.ci.entities;
 
 public class Orange {
 	
-	private String appName = "Good orange";
+	private String appName = "Good orange ||| Test Jenkins ...";
 	
 	public String getAppName() {
 		return appName;
